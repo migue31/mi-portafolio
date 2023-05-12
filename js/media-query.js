35db63bb97a3a8b2b6d56644eb04eba7533613da
migue31 <maortiz1503@soy.sena.edu.co>
2023-05-12 11:47:35 -0500

    map78

--- js/media-query.js
+++ js/media-query.js
@@ -1,5 +1,20 @@
-let menuBar = document.getElementById("menuBar");
+ let menuBar = document.getElementById("menuBar");
+let mainMenu = document.getElementById("mainMenu");
 
 menuBar.addEventListener("click", function() {
-    alert("Holiiii, si funciona!! :)");
+/*       alert("Holiiii, si funciona!! :)"); */
+    if (mainMenu.classList.contains("main-menu")) {
+        mainMenu.classList.add("main-menu-block");
+        mainMenu.classList.remove("main-menu");
+    }
+     else {
+        mainMenu.classList.add("main-menu");
+        mainMenu.classList.remove("main-menu-block");
+
+    }    
 });
+ /*    mainNAv.classList.remove("main-nav");
+    mainNAv.classList.add("main-nav-2") */;
+    /* mainMenu.classList.remove("main-menu")
+    mainMenu.classList.add("main-menu-block") */
+    
